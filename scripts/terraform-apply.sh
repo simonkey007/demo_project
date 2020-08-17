84e84e53e4bdfbea3100daf16fd842795b333152
Szymon Kodrebski <simonkey007@gmail.com>
2020-08-17 10:21:46 +0200

    Added terraform plan

--- scripts/terraform-apply.sh
+++ scripts/terraform-apply.sh
@@ -6,4 +6,5 @@ export TF_IN_AUTOMATION="true"
 
 cd ${TERRAFORM_DIRECTORY}
 terraform init -backend-config="${TERRAFORM_CONFIG}"
+terraform plan -var-file="${TERRAFORM_TFVARS}"
 terraform apply -auto-approve -var-file="${TERRAFORM_TFVARS}"
